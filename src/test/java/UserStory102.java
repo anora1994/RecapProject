e85bd090fb3672fceac144e18e0c3adff3f3431f
anora1994 <82920621+anora1994@users.noreply.github.com>
2021-07-06 23:21:11 -0400

    Us102 has been completed (#2)
    Combine all 3 commits by squashing it

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,7 @@
+public class UserStory102 {
+    //lots of code has been written here
+    //redy to push this brunch to remote
+    //and open pull request so I can start collaborating
+    //and my team member can comment, review or request for more changes
+    //more changes added as requested
+}
