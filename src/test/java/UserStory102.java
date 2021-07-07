19ed7715d41b29dfac1bae527e89733813dc58a5
anora1994 <anoragranat@gmail.com>
2021-07-06 23:14:13 -0400

    User story 102 has finished

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,2 +1,6 @@
 public class UserStory102 {
+    //lots of code has been written here
+    //redy to push this brunch to remote
+    //and open pull request so I can start collaborating
+    //and my team member can comment, review or request for more changes
 }
